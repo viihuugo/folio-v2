dcfbce4e1ba2f3cbc92c7834ac7ff44e6d569454
Victor Alves <hello@victrhugoalves.com>
2023-06-17 16:53:47 -0300

    teste

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react()],
   server: {
     // This configuration is required for client-side rendering
     historyApiFallback: true,
