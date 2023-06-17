f670ba2b16d2be9944763a5d4830192d9d2498f5
Victor Alves <hello@victrhugoalves.com>
2023-06-17 16:51:23 -0300

    fix routes

--- src/components/ScrollToTop/index.js
+++ src/components/ScrollToTop/index.js
@@ -3,11 +3,11 @@ import { useLocation } from "react-router-dom";
 
 export default function ScrollToTop() {
 
-    const { pathname } = useLocation();
+    const location = useLocation();
 
     useEffect(()=>{
         window.scrollTo(0,0);        
-    }, [pathname]);
+    }, [location]);
 
   return null;
 }
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    // This configuration is required for client-side rendering
+    historyApiFallback: true,
+  },
+});
